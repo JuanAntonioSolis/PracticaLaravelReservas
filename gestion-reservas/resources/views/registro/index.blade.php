884b0c0d01beb87dfb8f13d6a8b3dc3c908f53be
martinalejandroo25 <martinalejandroo25@gmail.com>
2025-11-27 14:52:31 +0100

    añadido y configuracion de frameworks bootstrap, fontawesome, etc

--- gestion-reservas/resources/views/registro/index.blade.php
+++ gestion-reservas/resources/views/registro/index.blade.php
@@ -1,29 +1,96 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <title>Registros</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Gestion Reservas - Login</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
 </head>
-<body>
-    <h1 class="tittle">Registro inicial</h1>
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+
+<body class="backdrop-grayscale-50">
+
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+
+                        <div class="mb-md-5 mt-md-4 pb-5">
+
+<!-- Recogida de datos para registro de  usuarios: Nombre email password telefono -->
+                            <h2 class="fw-bold mb-2 text-uppercase">Registro</h2>
+                            <p class="text-white-50 mb-5">Registro de nuevo usuario</p>
+
+                            <div data-mdb-input-init class="form-outline form-white mb-4">
+                                <input type="text" id="nombre" class="form-control form-control-lg" />
+                                <label class="form-label" for="nombre">Nombre Completo</label>
+                            </div>
+
+                            <div data-mdb-input-init class="form-outline form-white mb-4">
+                                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                                <label class="form-label" for="typeEmailX">Email</label>
+                            </div>
+
+                            <div data-mdb-input-init class="form-outline form-white mb-4">
+                                <input type="tel" id="telefono" class="form-control form-control-lg" />
+                                <label class="form-label" for="telefono">Telefono</label>
+                            </div>
+
+                            <div data-mdb-input-init class="form-outline form-white mb-4">
+                                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                                <label class="form-label" for="typePasswordX">Password</label>
+                            </div>
+
+                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+                            <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                            </div>
+
+                            {{-- Test Bootstrap Button and FontAwesome Icon --}}
+                            <div class="mt-4">
+                                <button type="button" class="btn btn-primary">
+                                    <i class="fas fa-check"></i> Test Button
+                                </button>
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <p class="mb-0">No tiene una cuenta ? <a href="/registro" class="text-white-50 fw-bold">Registrarse </a>
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
+</section>
+
+{{-- Removed old script imports, as they are now handled by Vite --}}
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script>
+
 </body>
+
 </html>
